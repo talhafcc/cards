b60cbba2aa84df4226514a8f3192aa5e38a3751a
Talha Khan <talhaahmedk@yahoo.ca>
2026-04-05 02:42:31 -0600

    cleanup

--- api/gameplay/deck.js
+++ api/gameplay/deck.js
@@ -1,18 +1,5 @@
 var shuffle = require('mess');
 
-// function shuffle(card, n) {       
-//     var test = card.slice()
-//     for (var i = 0; i < n; i++) { 
-//         // Random for remaining positions. 
-//         var r = i + Math.floor(Math.random() * Math.floor(52 - i))
-        
-//         //swapping the elements 
-//         var temp = test[r]; 
-//         test[r] = test[i]; 
-//         test[i] = temp; 
-//     } 
-//     return(test)
-// } 
 
 var deck = {
     cards: function() {
@@ -21,7 +8,6 @@ var deck = {
                         'H02','H03','H04','H05','H06','H07','H08','H09','H10','H11','H12','H13','H14',
                         'S02','S03','S04','S05','S06','S07','S08','S09','S10','S11','S12','S13','S14' 
                     ];
-        // var cards = shuffle(myCards, 52)
         return deck.distribute(shuffle(myCards, 52));
     },
 
@@ -36,10 +22,6 @@ var deck = {
                 }
             }
         }
-        // pcards.p1 = arr.splice(0,13)
-        // pcards.p2 = arr.splice(0,13)
-        // pcards.p3 = arr.splice(0,13)
-        // pcards.p4 = arr.splice(0,13)
 
         shuffled_deck = (pcards.p1).concat(pcards.p2).concat(pcards.p3).concat(pcards.p4);
         return shuffled_deck;
